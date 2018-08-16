ea2e34ae96591bd3c37ffa5b1a134a7695b56230
Hoegnison <smarttactics101@googlemail.com>
2018-08-16 23:10:31 +0200

    Reverted Folder Split
    Reverted Folder Split Because Of CI Config

--- vanilla/AAW_AAS_NATO_CSAT_OpDeepWater.Malden/cfgGroupTypes.hpp
+++
@@ -1,9 +0,0 @@
-class CfgGroupTypes {
-    activeGroupTypes[] = {"Rifle", "Weapon", "Vehicle", "Helicopter"};
-    class Vehicle {
-        requiredPlayers = 15;
-    };
-    class Helicopter {
-        requiredPlayers = 20;
-    };
-};
--- vanilla/AAW_DEV_OpDevelopment.Stratis/cfgRespawn.hpp
+++
@@ -1,4 +0,0 @@
-class CfgRespawn {
-    matchStartCountdown = 5; // Time until match starts (after mission start)
-    respawnCountdown = 3; // Time until respawn is available
-};
