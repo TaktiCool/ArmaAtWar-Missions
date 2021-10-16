b99f1e9e4bfeb501de91765d4719ea117c82aaef
jokoho482 <hoffmann.jonas95@gmail.com>
2021-10-17 00:43:02 +0200

    Fix Missing BaseClasses in one Mission

---
+++ AAW_AAS_WM_US_Trenches.I44_Omaha_V2/ui/common/baseclasses.hpp
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////////
+/// Importing Base Classes
+///////////////////////////////////////////////////////////////////////////
+import ScrollBar;
+
+// ControlsGroup
+import RscControlsGroup;
+import RscControlsGroupNoScrollbars;
+
+// Lists
+import RscListBox;
+import RscListNBox;
+
+// Texts
+import RscText;
+import RscStructuredText;
+
+
+import RscPicture;
+
+
+import RscEdit;
+import RscCombo;
+
+import RscButton;
+
+import RscMapControl;
+
+import RscProgress;
+
+import RscToolbox;
